be1d99ea91225cccc86e4ec9fe673fb26afd9047
Adam Mckaig <adam.mckaig@gmail.com>
2009-01-16 11:10:23 -0500

    fixed pilot gmc uids, and added cheat-sheet example

--- bin/migrate.rb
+++ bin/migrate.rb
@@ -16,6 +16,15 @@ DataMapper.auto_migrate!
 
 # create the pilot districts and gmcs
 Gmc.create(:district => District.create(:title => "Kasungu"), :uid => 1001, :title => "Tamani")
-Gmc.create(:district => District.create(:title => "Salima"),  :uid => 1001, :title => "Chipoka")
-Gmc.create(:district => District.create(:title => "Dedza"),   :uid => 1001, :title => "Chikuse")
+Gmc.create(:district => District.create(:title => "Salima"),  :uid => 1101, :title => "Chipoka")
+Gmc.create(:district => District.create(:title => "Dedza"),   :uid => 1201, :title => "Chikuse")
+
+# create the example district and gmc, as
+# shown on the cheat-sheets and posters
+Gmc.create(
+	:uid => 1234,
+	:title => "Example GMC",
+	:district => District.create(
+		:title => "Example District"))
+
 puts "Migrated."
