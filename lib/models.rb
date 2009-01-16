3ca2d676d6a8561b9827a9be963cb45cbd8dc690
Adam Mckaig <adam.mckaig@gmail.com>
2009-01-16 12:59:46 -0500

    tidied up models.rb

--- lib/models.rb
+++ lib/models.rb
@@ -19,6 +19,7 @@ class District
 	property :title, String, :length => 60
 end
 
+
 class Gmc
 	include DataMapper::Resource
 	belongs_to :district
@@ -30,6 +31,7 @@ class Gmc
 	
 	property :title, String, :length => 60
 end
+
 
 class Child
 	include DataMapper::Resource
@@ -45,18 +47,21 @@ class Child
 	property :contact, String, :length=>22
 end
 
+
 class Report
 	include DataMapper::Resource
+	belongs_to :child
+	
 	property :id, Integer, :serial=>true
+	
 	property :weight, Float 
 	property :height, Float 
 	property :muac, Float
-	property :oedema, Boolean, :default => false
+	property :oedema, Boolean
 	property :diarrhea, Boolean
 	
 	property :sent, DateTime
 	property :received, DateTime
-	belongs_to :child
 	
 	# Returns the weight to height ratio of
 	# this report, or nil, if either fields
@@ -67,11 +72,21 @@ class Report
 		(w && h) ? sprintf("%.2f", w/h).to_f : nil
 	end
 
+	# Returns _true_ if this report indicates a moderately malnourished
+	# child. Note that _false_ is returned if we cannot tell (due to the
+	# _ratio_ or _muac_ being nil), the child is severely malnourished,
+	# or the child is not malnourished at all.
 	def moderate?
-		(!ratio.nil? and ratio > 0.70 and ratio < 0.79) or (!muac.nil? and muac > 11.0 and muac < 11.9)
+		(!ratio.nil? and ratio > 0.70 and ratio < 0.79)\
+		or (!muac.nil? and muac > 11.0 and muac < 11.9)
 	end
 
+	# Returns true if this report indicates a severely malnourished
+	# child, or false if we cannot tell (due to _ratio_ or _muac_
+	# being nil), or the child is not severely malnourished.
 	def severe?
-		(!ratio.nil? and ratio <= 0.70) or (!muac.nil? and muac < 11.0) or (oedema)
+		(!ratio.nil? and ratio <= 0.70)\
+		or (!muac.nil? and muac <= 11.0)\
+		or (oedema)
 	end
 end
