c142b74c971f114c32bcd2247e1199114a97ce71
Adam Mckaig <adam.mckaig@gmail.com>
2009-01-21 09:28:34 -0500

    added descriptive labels to data tables

--- lib/webui/app/controllers/dashboard.rb
+++ lib/webui/app/controllers/dashboard.rb
@@ -1,14 +1,16 @@
 class Dashboard < Application
 	
   def index
+    @data_from = "all GMCs"
     @reports = Report.all(:order => [:date.desc], :cancelled=>[true, false])
-    @children = Child.all
+    @children = Child.all(:order => [:id.desc])
     render :index
   end
   
   def district
   	fetch_district
   	
+  	@data_from = "all GMCs in #{@district.title} District"
   	@reports = Report.all("child.gmc.district.id" => @district.id)
   	@children = Child.all("gmc.district.id" => @district.id)
   	render :index
@@ -18,8 +20,10 @@ class Dashboard < Application
   	fetch_district
   	fetch_gmc
   	
+  	@data_from = "#{@gmc.title} GMC in #{@district.title} District"
   	@reports = Report.all("child.gmc.id" => @gmc.id)
   	@children = Child.all("gmc.id" => @gmc.id)
+  	
   	render :index
   end
   
